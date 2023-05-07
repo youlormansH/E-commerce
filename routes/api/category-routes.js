63bc1d9693d79b82fd06b65524bab5a35bef34b3
youlormansH <128164751+youlormansH@users.noreply.github.com>
2023-05-07 12:02:07 -0400

    route.post

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -50,6 +50,14 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+  .then(rgCategoryData => res.json(rgCategoryData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.put('/:id', async (req, res) => {
